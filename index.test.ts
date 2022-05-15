6c5fea9442d74c94d023942e953157cf19d395d2
Josh <jm8hjkl@gmail.com>
2022-05-15 14:19:36 -0400

    get possible moves

--- index.test.ts
+++ index.test.ts
@@ -1,8 +1,73 @@
-import {getEffectiveSuit} from './index'
+import { getEffectiveSuit, getPossibleMoves, Card } from './index'
 
 test('effective suit', () => {
-  expect(getEffectiveSuit('3H', {color: 'red', parity: 'even'})).toBe('H')
-  expect(getEffectiveSuit('3H', {color: 'red', parity: 'odd'})).toBe('T')
-  expect(getEffectiveSuit('6C', {color: 'red', parity: 'even'})).toBe('C')
-  expect(getEffectiveSuit('J', {color: 'red', parity: 'even'})).toBe('T')
+  expect(getEffectiveSuit('3H', { color: 'red', parity: 'even' })).toBe('H')
+  expect(getEffectiveSuit('3H', { color: 'red', parity: 'odd' })).toBe('T')
+  expect(getEffectiveSuit('6C', { color: 'red', parity: 'even' })).toBe('C')
+  expect(getEffectiveSuit('J', { color: 'red', parity: 'even' })).toBe('T')
+})
+
+test('forced to follow suit', () => {
+  expect(getPossibleMoves({
+    leader: 0,
+    won: [],
+    played: ['5H'],
+    hand: new Set<Card>([
+      '4H',
+      '3H',
+      '2D',
+      'G',
+      '5C',
+      '3S',
+      '2C',
+      '4S',
+    ]),
+  }, { color: 'red', parity: 'even' })).toEqual(new Set<Card>([
+    '3H',
+  ]))
+})
+
+test('forced to play trump', () => {
+  expect(getPossibleMoves({
+    leader: 0,
+    won: [],
+    played: ['2H'],
+    hand: new Set<Card>([
+      '4H',
+      '3H',
+      '2D',
+      'G',
+      '5C',
+      '3S',
+      '2C',
+      '4S',
+    ]),
+  }, { color: 'red', parity: 'even' })).toEqual(new Set<Card>([
+    '4H',
+    '2D',
+    'G',
+  ]))
+})
+
+test('play whatever', () => {
+  expect(getPossibleMoves({
+    leader: 0,
+    won: [],
+    played: ['3S'],
+    hand: new Set<Card>([
+      '4H',
+      '3H',
+      '2D',
+      'G',
+      '5C',
+      '2C',
+    ]),
+  }, { color: 'red', parity: 'even' })).toEqual(new Set<Card>([
+    '4H',
+    '3H',
+    '2D',
+    'G',
+    '5C',
+    '2C',
+  ]))
 })
